f311afee074e3a6bccbd1a3d6e52e3c6026313bc
Gerald Hernandez <gerald2557@gmail.com>
2017-01-01 05:35:43 +0000

    Book images now showing up dynamicaly

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   before_action :authenticate_user!, only: :dashboard
   def home
     redirect_to dashboard_path if current_user
+    @books = Book.last(4)
   end
 
   def dashboard
